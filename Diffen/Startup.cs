d7c16709f77d5651bde924718a885d5fc0af6f10
Simon Rudén <ruden.simon@gmail.com>
2018-03-31 14:20:32 +0200

    install serilog and setup environment specific configuration files

--- Diffen/Startup.cs
+++ Diffen/Startup.cs
@@ -5,12 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Diffen
 {
 	public class Startup
 	{
+		public IConfigurationRoot Configuration { get; }
+
+		public Startup(IHostingEnvironment environment)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(environment.ContentRootPath)
+				.AddJsonFile($"appsettings.{environment.EnvironmentName}.json")
+				.AddEnvironmentVariables();
+
+			Configuration = builder.Build();
+		}
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
