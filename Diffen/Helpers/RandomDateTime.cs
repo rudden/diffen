7ea65f9ffc64712fc72cf9d92d0e83a9d68a444b
Simon Rudén <ruden.simon@gmail.com>
2018-04-02 15:31:49 +0200

    Minor modification to RandomDateTime class

--- Diffen/Helpers/RandomDateTime.cs
+++ Diffen/Helpers/RandomDateTime.cs
@@ -6,20 +6,18 @@ namespace Diffen.Helpers
 	{
 		public static DateTime Get()
 		{
-			var start = new DateTime(2017, 12, 1);
-			var end = new DateTime(2018, 6, 1);
-
-			var rand = new Random();
-			var range = (end - start).Days;
-
-			return start.AddDays(rand.Next(range)).AddHours(rand.Next(0, 24)).AddMinutes(rand.Next(0, 60)).AddSeconds(rand.Next(0, 60));
+			return Randomize(new DateTime(2017, 12, 1), new DateTime(2018, 6, 1));
 		}
 
 		public static DateTime Get(DateTime start, DateTime end)
 		{
+			return Randomize(start, end);
+		}
+
+		public static DateTime Randomize(DateTime start, DateTime end)
+		{
 			var rand = new Random();
 			var range = (end - start).Days;
-
 			return start.AddDays(rand.Next(range)).AddHours(rand.Next(0, 24)).AddMinutes(rand.Next(0, 60)).AddSeconds(rand.Next(0, 60));
 		}
 	}
