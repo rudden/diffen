287b0789f5fcd6407b08bfe3660c33e7b239578b
Simon Rudén <ruden.simon@gmail.com>
2018-04-02 15:30:07 +0200

    Model for parent posts in conversation

---
+++ Diffen/Models/Forum/ParentPost.cs
@@ -0,0 +1,10 @@
+﻿namespace Diffen.Models.Forum
+{
+	public class ParentPost
+	{
+		public int Id { get; set; }
+		public string Message { get; set; }
+		public User User { get; set; }
+		public string Since { get; set; }
+	}
+}
