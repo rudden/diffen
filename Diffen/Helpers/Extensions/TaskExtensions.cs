593d16f19cab86b8eb909fb2b10aac84f3338433
Simon Rudén <ruden.simon@gmail.com>
2018-04-03 20:46:19 +0200

    Task extension class
    This class has a method that is used to act upon the returned
    boolean value of a crud operation in the db repositories.
    It takes two parameters, an item with a message value for both
    a success (true) or failure (false) and the current results list.
    Since some requests do multiple actions some can be successful
    while others can fail.

---
+++ Diffen/Helpers/Extensions/TaskExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Diffen.Helpers.Extensions
+{
+	using Models;
+
+	public static class TaskExtensions
+	{
+		public static void UpdateResults(this Task<bool> task, Message message, ICollection<Result> results)
+		{
+			var result = new Result
+			{
+				Type = (ResultType)Convert.ToInt32(task.Result)
+			};
+			switch (result.Type)
+			{
+				case ResultType.Failure:
+					result.Message = message.Failure;
+					break;
+				case ResultType.Success:
+					result.Message = message.Success;
+					break;
+			}
+			results.Add(result);
+		}
+	}
+}
