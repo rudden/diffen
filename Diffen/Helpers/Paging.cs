c77ef09a73602ea6bf0030a47fb9751af9157b61
Simon Rudén <ruden.simon@gmail.com>
2018-04-03 20:48:27 +0200

    A class used when returning paged items

---
+++ Diffen/Helpers/Paging.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Diffen.Helpers
+{
+	public class Paging<T>
+	{
+		public IEnumerable<T> Data { get; set; }
+		public int CurrentPage { get; set; }
+		public int NumberOfPages { get; set; }
+		public int Total { get; set; }
+
+		public Paging() { }
+		public Paging(IEnumerable<T> data)
+		{
+			Data = data;
+		}
+	}
+}
