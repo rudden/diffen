2abf1f1224bf69bcf1bbe26a0621bee217e3a982
Simon Rudén <ruden.simon@gmail.com>
2018-04-07 14:18:53 +0200

    Remove redundant .js files generated by webpack
    Accidentaly added these in last commit

--- Diffen/src/scripts/modules/profile/mutations.js
+++
@@ -1,7 +0,0 @@
-import { SET_USER, SET_KVP_USERS } from './types';
-export var Mutations = (_a = {},
-    _a[SET_USER] = function (state, user) { state.user = user; },
-    _a[SET_KVP_USERS] = function (state, users) { state.users = users; },
-    _a);
-export default Mutations;
-var _a;
--- Diffen/src/scripts/modules/profile/state.js
+++
@@ -1,9 +0,0 @@
-import { User } from '../../model/profile';
-var State = /** @class */ (function () {
-    function State() {
-        this.user = new User();
-        this.users = [];
-    }
-    return State;
-}());
-export default State;
