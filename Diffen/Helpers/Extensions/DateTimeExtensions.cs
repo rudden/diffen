fb118af897b32a20689aff1bee044e61b59a9a23
Simon Rudén <ruden.simon@gmail.com>
2018-04-07 12:23:33 +0200

    Removing unnecessary namespacing for DateTime class

--- Diffen/Helpers/Extensions/DateTimeExtensions.cs
+++ Diffen/Helpers/Extensions/DateTimeExtensions.cs
@@ -5,40 +5,40 @@ namespace Diffen.Helpers.Extensions
 {
 	public static class DateTimeExtensions
 	{
-		public static string GetSinceStamp(this System.DateTime dt)
+		public static string GetSinceStamp(this DateTime dt)
 		{
-			if (dt < System.DateTime.Now.AddDays(-1))
+			if (dt < DateTime.Now.AddDays(-1))
 			{
-				if (Convert.ToInt32((System.DateTime.Now - dt).Days) > 7)
+				if (Convert.ToInt32((DateTime.Now - dt).Days) > 7)
 				{
 					return dt.ToString("yyyy-MM-dd HH:mm");
 				}
-				return (System.DateTime.Now - dt).Days.ToString(CultureInfo.InvariantCulture) + " d sedan";
+				return (DateTime.Now - dt).Days.ToString(CultureInfo.InvariantCulture) + " d sedan";
 			}
-			if (dt > System.DateTime.Now.AddHours(-1))
+			if (dt > DateTime.Now.AddHours(-1))
 			{
-				var minutes = (System.DateTime.Now - dt).Minutes;
+				var minutes = (DateTime.Now - dt).Minutes;
 				if (minutes <= 0)
 				{
 					return "alldeles nyss";
 				}
-				return (System.DateTime.Now - dt).Minutes.ToString(CultureInfo.InvariantCulture) + " m sedan";
+				return (DateTime.Now - dt).Minutes.ToString(CultureInfo.InvariantCulture) + " m sedan";
 			}
-			if (dt > System.DateTime.Now.AddDays(-1))
+			if (dt > DateTime.Now.AddDays(-1))
 			{
-				return (System.DateTime.Now - dt).Hours.ToString(CultureInfo.InvariantCulture) + " t sedan";
+				return (DateTime.Now - dt).Hours.ToString(CultureInfo.InvariantCulture) + " t sedan";
 			}
 			return "";
 		}
 
-		public static string GetSinceStamp(this System.DateTime? dtn)
+		public static string GetSinceStamp(this DateTime? dtn)
 		{
 			return dtn == null ? null : Convert.ToDateTime(dtn).GetSinceStamp();
 		}
 
-		public static string GetSecluded(this System.DateTime dt)
+		public static string GetSecluded(this DateTime dt)
 		{
-			return dt < System.DateTime.Now ? "" : dt.ToString("yyyy-MM-dd");
+			return dt < DateTime.Now ? "" : dt.ToString("yyyy-MM-dd");
 		}
 	}
 }
