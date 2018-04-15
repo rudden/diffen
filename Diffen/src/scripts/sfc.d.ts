75b38c658c13dccc1fe68ba70400bd03dccdf3b2
Simon Rudén <ruden.simon@gmail.com>
2018-04-15 12:31:41 +0200

    Install spinner package as an accessible module

--- Diffen/src/scripts/sfc.d.ts
+++ Diffen/src/scripts/sfc.d.ts
@@ -5,4 +5,5 @@ declare module "*.vue" {
 declare module 'vue-pagination-2'
 declare module 'vue-tweet-embed'
 declare module 'vue-bootstrap-datetimepicker'
+declare module 'vue-loading-spinner'
 declare module 'uiv'
