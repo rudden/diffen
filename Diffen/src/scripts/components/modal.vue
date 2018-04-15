6eb7091a727bfe4ecb9eb6fdc95be3a5e2010f0c
Simon Rudén <ruden.simon@gmail.com>
2018-04-15 13:50:46 +0200

    Changes to generic modal component
    Now has a slot for button as well

--- Diffen/src/scripts/components/modal.vue
+++ Diffen/src/scripts/components/modal.vue
@@ -1,19 +1,15 @@
 <template>
     <div class="wrap">
-        <small>
-            <a href="#" data-toggle="modal" :data-target="'#' + id">{{ btnText }}</a>
-        </small>
+        <slot name="btn"></slot>
         <div class="modal fade" :id="id" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">{{ header }}</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click="closeAction ? closeAction : null">&times;</button>
                     </div>
-                    <div class="modal-body p-0">
-                        <div class="modal-body">
-                            <slot name="body"></slot>
-                        </div>
+                    <div class="modal-body">
+                        <slot name="body"></slot>
                     </div>
                 </div>
             </div>
@@ -25,18 +21,16 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 
-import {  } from '../../../modules/forum/types'
-
 @Component({
     props: {
         id: String,
         header: String,
-        btnText: String
+        closeAction: Function
     }
 })
 export default class Modal extends Vue {
     id: string
     header: string
-    btnText: string
+    closeAction: Function
 }
 </script>
