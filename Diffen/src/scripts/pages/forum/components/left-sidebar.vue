c4e2d95bbd1e8d8df2de86986ac5ae6d3524f3be
Simon Rudén <ruden.simon@gmail.com>
2018-04-15 13:56:00 +0200

    Add some more information to the left sidebar for forum page
    Now shows some information about the logged in user and the
    url tip click count toplist for the last month

--- Diffen/src/scripts/pages/forum/components/left-sidebar.vue
+++ Diffen/src/scripts/pages/forum/components/left-sidebar.vue
@@ -1,23 +1,50 @@
 <template>
-    <div class="col-lg-3">
+    <div class="col-lg-3 d-none d-lg-block">
         <div class="card card-profile mb-4">
             <div class="card-header" style="background-image: url(stadion.png);"></div>
             <div class="card-body text-center">
                 <a href="/profile">
-                    <img class="card-profile-img" :src="vm.loggedInUser.avatar">
+                    <img class="card-profile-img" :src="user.avatar">
                 </a>
                 <h6 class="card-title">
-                    <a class="text-inherit" href="/profile">{{ vm.loggedInUser.nickName }}</a>
+                    <a class="text-inherit" href="/profile">{{ user.nickName }}</a>
                 </h6>
-                <p class="mb-4">{{ vm.loggedInUser.bio }}</p>
+                <p class="mb-4">{{ user.bio }}</p>
                 <ul class="card-menu">
                     <li class="card-menu-item">
                         inlägg
-                        <h6 class="my-0">{{ vm.loggedInUser.numberOfPosts }}</h6>
+                        <h6 class="my-0">{{ user.numberOfPosts }}</h6>
                     </li>
                     <li class="card-menu-item">
                         karma
-                        <h6 class="my-0">{{ vm.loggedInUser.karma }}</h6>
+                        <h6 class="my-0">{{ user.karma }}</h6>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="card mb-4 mb-4">
+            <div class="card-body">
+                <h6 class="mb-3">om</h6>
+                <ul class="list-unstyled list-spaced mb-0">
+                    <li><span class="text-muted icon icon-thumbs-up mr-3"></span>{{ user.voteStatistics.upVotes }} givna upptummar</li>
+                    <li><span class="text-muted icon icon-thumbs-down mr-3"></span>{{ user.voteStatistics.downVotes }} givna nertummar</li>
+                    <template v-if="user.savedPostsIds">
+                        <li><span class="text-muted icon icon-bookmark mr-3"></span>{{ user.savedPostsIds.length }} sparade inlägg</li>
+                    </template>
+                    <template v-if="user.favoritePlayer">
+                        <li><span class="text-muted icon icon-heart mr-3"></span>{{ user.favoritePlayer.fullName }}</li>
+                    </template>
+                </ul>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="card-body">
+                <h6 class="mb-3">länktipstoppen</h6>
+                <ul class="list-unstyled list-spaced mb-0" v-if="urlTips.length > 0">
+                    <li v-for="tip in urlTips" v-bind:key="tip.href">
+                        <span class="text-muted icon" :class="icon(tip.href)"></span>
+                        <small class="ml-1"><span class="badge badge-secondary">{{ tip.clicks }}</span></small>
+                        <url :href="tip.href" :post-id="tip.postId" class="ml-1" />
                     </li>
                 </ul>
             </div>
@@ -28,13 +55,61 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import { State } from 'vuex-class'
+import { Action, State, namespace } from 'vuex-class'
 
-import { Post, Vote, VoteType } from '../../../model/forum'
 import { ViewModel } from '../../../model/common'
+import { UrlTip } from '../../../model/forum'
 
-@Component({})
+const ModuleAction = namespace('forum', Action)
+
+import { FETCH_URLTIP_TOPLIST } from '../../../modules/forum/types'
+
+import Url from '../../../components/url.vue'
+
+@Component({
+    components: {
+        Url
+    }
+})
 export default class LeftSidebar extends Vue {
     @State(state => state.vm) vm: ViewModel
+    @ModuleAction(FETCH_URLTIP_TOPLIST) loadUrlTipTopList: () => Promise<UrlTip[]>
+
+    urlTips: UrlTip[] = []
+    
+    mounted() {
+        this.loadUrlTipTopList()
+            .then((topList: UrlTip[]) => this.urlTips = topList)
+    }
+
+    get user() {
+        return this.vm.loggedInUser
+    }
+
+    icon(href: string): string {
+        if (href.includes('facebook'))
+            return 'icon-facebook'
+        if (href.includes('instagram'))
+            return 'icon-instagram'
+        if (href.includes('twitter'))
+            return 'icon-twitter'
+        if (href.includes('youtube'))
+            return 'icon-youtube'
+        if (href.includes('spotify'))
+            return 'icon-spotify'
+        if (href.includes('soundcloud'))
+            return 'icon-soundcloud'
+        if (href.includes('dif'))
+            return 'icon-heart'
+        return 'icon-link'
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+li {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+</style>
