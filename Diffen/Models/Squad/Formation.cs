c0f4c56687f5fb9d833ac1fcb1714363eb6e6339
Simon Rudén <ruden.simon@gmail.com>
2018-04-15 13:40:00 +0200

    Models used to return formations and positions from api

---
+++ Diffen/Models/Squad/Formation.cs
@@ -0,0 +1,9 @@
+﻿namespace Diffen.Models.Squad
+{
+	public class Formation
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string ComponentName { get; set; }
+	}
+}
---
+++ Diffen/Models/Squad/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace Diffen.Models.Squad
+{
+	public class Position
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
