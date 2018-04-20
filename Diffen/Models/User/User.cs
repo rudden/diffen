cd7eb226648742236793caa9fbfa92bcddfcccf0
Simon Rudén <ruden.simon@gmail.com>
2018-04-20 13:19:11 +0200

    Include email on user model

--- Diffen/Models/User/User.cs
+++ Diffen/Models/User/User.cs
@@ -8,6 +8,7 @@ namespace Diffen.Models.User
 	{
 		public string Id { get; set; }
 		public string Bio { get; set; }
+		public string Email { get; set; }
 		public string NickName { get; set; }
 		public string Avatar { get; set; }
 
