71f42480c3b7de42f51bad3c4f8e06210865bbc9
Simon Rudén <ruden.simon@gmail.com>
2018-04-20 12:30:45 +0200

    Call specific action when closing modal from generic modal component

--- Diffen/src/scripts/components/modal.vue
+++ Diffen/src/scripts/components/modal.vue
@@ -6,7 +6,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h4 class="modal-title">{{ header }}</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click="closeAction ? closeAction : null">&times;</button>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click="close">&times;</button>
                     </div>
                     <div class="modal-body">
                         <slot name="body"></slot>
@@ -32,5 +32,11 @@ export default class Modal extends Vue {
     id: string
     header: string
     closeAction: Function
+
+    close() {
+        if (this.closeAction) {
+            this.closeAction()
+        }
+    }
 }
 </script>
