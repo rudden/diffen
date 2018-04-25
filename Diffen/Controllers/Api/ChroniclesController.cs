8b17fddaf07971e86f0c0b05167323066b8c7a18
Simon Rudén <ruden.simon@gmail.com>
2018-04-25 19:16:14 +0200

    Api controllers for polls and chronicles

---
+++ Diffen/Controllers/Api/ChroniclesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+using Serilog;
+
+namespace Diffen.Controllers.Api
+{
+	using Models;
+	using Models.Other;
+	using Repositories.Contracts;
+
+	[Route("api/[controller]")]
+	public class ChroniclesController : Controller
+	{
+		private readonly IChronicleRepository _chronicleRepository;
+
+		private readonly ILogger _logger = Log.ForContext<ChroniclesController>();
+
+		private const string DirectoryForChronicleHeaderUploads = "uploads/chronicles";
+
+		public ChroniclesController(IChronicleRepository chronicleRepository, IHostingEnvironment environment)
+		{
+			_chronicleRepository = chronicleRepository;
+		}
+
+		[HttpGet]
+		public Task<List<Chronicle>> Get()
+		{
+			_logger.Debug("Requesting all chronicles");
+			return _chronicleRepository.GetAllChroniclesAsync();
+		}
+
+		[HttpGet("{userId}")]
+		public Task<List<Chronicle>> GetOnUserId(string userId)
+		{
+			_logger.Debug("Requesting all chronicles created by user with id {userId}", userId);
+			return _chronicleRepository.GetAllChroniclesOnUserIdAsync(userId);
+		}
+
+		[HttpGet("{slug}")]
+		public Task<Chronicle> Get(string slug)
+		{
+			_logger.Debug("Requesting chronicle with slug {slug}", slug);
+			return _chronicleRepository.GetChronicleOnSlugAsync(slug);
+		}
+
+		[HttpPost("create")]
+		public Task<List<Result>> Create([FromBody] Models.Other.CRUD.Chronicle chronicle)
+		{
+			_logger.Debug("Requesting to create a new chronicle");
+			return _chronicleRepository.CreateChronicleAsync(chronicle);
+		}
+
+		[HttpPost("update")]
+		public Task<List<Result>> Update([FromBody] Models.Other.CRUD.Chronicle chronicle)
+		{
+			_logger.Debug("Requesting to update an existing chronicle");
+			return _chronicleRepository.UpdateChronicleAsync(chronicle);
+		}
+
+		[HttpPost("image/header/update/{fileName}")]
+		public async Task<bool> UpdateHeaderFileName(string fileName)
+		{
+			_logger.Debug("Requesting to update an existing chronicles header filename");
+			try
+			{
+				var isUpdated = await _chronicleRepository.UpdateHeaderFileNameForLastAddedChronicleAsync($"{DirectoryForChronicleHeaderUploads}/{fileName}");
+				return isUpdated.Any(x => x.Type == ResultType.Success);
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ Diffen/Controllers/Api/PollsController.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Serilog;
+
+namespace Diffen.Controllers.Api
+{
+	using Models;
+	using Models.Other;
+	using Repositories.Contracts;
+
+	[Route("api/[controller]")]
+	public class PollsController : Controller
+	{
+		private readonly IPollRepository _pollRepository;
+
+		private readonly ILogger _logger = Log.ForContext<PollsController>();
+
+		public PollsController(IPollRepository pollRepository)
+		{
+			_pollRepository = pollRepository;
+		}
+
+		[HttpGet]
+		public Task<List<Poll>> Get()
+		{
+			_logger.Debug("Requesting all polls");
+			return _pollRepository.GetAllPollsAsync();
+		}
+
+		[HttpGet("active")]
+		public Task<List<Poll>> GetActive(int amount)
+		{
+			_logger.Debug("Requesting all active polls");
+			return amount == 0
+				? _pollRepository.GetActivePollsAsync()
+				: _pollRepository.GetLastNthActivePollsAsync(amount);
+		}
+
+		[HttpGet("{userId}")]
+		public Task<List<Poll>> GetOnUserId(string userId)
+		{
+			_logger.Debug("Requesting all polls created by user with id {userId}", userId);
+			return _pollRepository.GetPollsForUserWithIdAsync(userId);
+		}
+
+		[HttpGet("{pollId}")]
+		public Task<Poll> Get(int pollId)
+		{
+			_logger.Debug("Requesting poll with id {pollId}", pollId);
+			return _pollRepository.GetPollOnIdAsync(pollId);
+		}
+
+		[HttpPost("create")]
+		public Task<List<Result>> Create([FromBody] Models.Other.CRUD.Poll poll)
+		{
+			_logger.Debug("Requesting to create a new poll");
+			return _pollRepository.CreateNewPollAsync(poll);
+		}
+
+		[HttpPost("{pollId}/vote")]
+		public Task<List<Result>> Vote(int pollId, [FromBody] Models.Other.CRUD.PollVote pollVote)
+		{
+			_logger.Debug("Requesting to create a new vote on poll with id {pollId}", pollId);
+			return _pollRepository.CreateVoteOnPollAsync(pollVote);
+		}
+	}
+}
