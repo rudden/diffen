90aa10b5ce1445f0f4ee43aa438873de87c686fc
Simon Rudén <ruden.simon@gmail.com>
2018-04-29 19:16:44 +0200

    Custom authorization attribute to verify input userid to loggedin userid

---
+++ Diffen/Helpers/Authorize/VerifyInputToLoggedInUserIdAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Diffen.Helpers.Authorize
+{
+	using Extensions;
+
+	public class VerifyInputToLoggedInUserIdAttribute : ActionFilterAttribute
+	{
+		private readonly string _requestUserId;
+
+		public VerifyInputToLoggedInUserIdAttribute(string requestUserId)
+		{
+			_requestUserId = requestUserId;
+		}
+
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			context.ActionArguments.TryGetValue(_requestUserId, out var value);
+			var requestUserId = value as string;
+
+			var loggedInUserId = context.HttpContext.User.GetUserId();
+			if (!string.IsNullOrWhiteSpace(requestUserId) && !string.IsNullOrWhiteSpace(loggedInUserId))
+			{
+				if (requestUserId.Equals(loggedInUserId))
+				{
+					base.OnActionExecuting(context);
+					return;
+				}
+			}
+			context.Result = new ForbidResult();
+		}
+	}
+}
