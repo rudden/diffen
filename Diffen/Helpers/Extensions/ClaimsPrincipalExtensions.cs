f2e8d9f3bf4367c5f6a4b676498637e3a7cbcf8f
Simon Rudén <ruden.simon@gmail.com>
2018-04-29 19:17:05 +0200

    Extension method to get user id of logged in user

---
+++ Diffen/Helpers/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Diffen.Helpers.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+		public static string GetUserId(this ClaimsPrincipal user)
+		{
+			return !user.Identity.IsAuthenticated ? null : user.FindFirst(ClaimTypes.NameIdentifier).Value;
+		}
+	}
+}
