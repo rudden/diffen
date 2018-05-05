503c3ee391dfa8cfcc1dd5ccb9938e4f9d9f458f
Simon Rudén <ruden.simon@gmail.com>
2018-05-05 13:32:43 +0200

    Cleanup using statements in region repot

--- Diffen/Repositories/RegionRepository.cs
+++ Diffen/Repositories/RegionRepository.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Linq;
+
 using AutoMapper;
 
 namespace Diffen.Repositories
