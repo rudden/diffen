58241fde103ace6b8b9e1b5e208a84689fb5486b
Simon Rudén <ruden.simon@gmail.com>
2018-05-09 15:05:26 +0200

    Set dynamic pagetitle

--- Diffen/Controllers/CommonController.cs
+++ Diffen/Controllers/CommonController.cs
@@ -27,17 +27,52 @@ namespace Diffen.Controllers
 
 		public override void OnActionExecuting(ActionExecutingContext context)
 		{
+			var controller = (ControllerBase)context.Controller;
+			var pageName = controller.ControllerContext.ActionDescriptor.ControllerName.ToLower();
 			if (Model == null)
 			{
-				var controller = (ControllerBase) context.Controller;
 				Model = new TModel
 				{
 					Api = _configuration["Api:Url"],
 					LoggedInUser = _mapper.Map<Models.User.User>(_userRepository.GetUserOnEmailAsync(User.Identity.Name).Result),
-					Page = controller.ControllerContext.ActionDescriptor.ControllerName.ToLower()
+					Page = pageName
 				};
 			}
+			SetPageTitle(pageName);
 			base.OnActionExecuting(context);
+		}
+
+		private void SetPageTitle(string pageName)
+		{
+			string pageTitle;
+			switch (pageName)
+			{
+				case "forum":
+					pageTitle = "Forum";
+					break;
+				case "chronicle":
+					pageTitle = "Krönikor";
+					break;
+				case "home":
+					pageTitle = "Hem";
+					break;
+				case "poll":
+					pageTitle = "Omröstningar";
+					break;
+				case "region":
+					pageTitle = "Områden";
+					break;
+				case "squad":
+					pageTitle = "Trupp";
+					break;
+				case "profile":
+					pageTitle = "Profil";
+					break;
+				default:
+					pageTitle = "";
+					break;
+			}
+			ViewBag.Title = $"{(!string.IsNullOrEmpty(pageTitle) ? "| " : "")} {pageTitle}";
 		}
 	}
 }
