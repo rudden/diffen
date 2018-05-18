b4eca9ea561f5c972656cf01a11f85b1de42e6f1
Simon Rudén <ruden.simon@gmail.com>
2018-05-18 17:31:20 +0200

    Some small adjustments to left sidebar
    Moved the urltip toplists to right sidebar instead

--- Diffen/src/scripts/pages/forum/components/left-sidebar.vue
+++ Diffen/src/scripts/pages/forum/components/left-sidebar.vue
@@ -4,13 +4,10 @@
             <div class="card-header" style="background-image: url(/bg.jpg);"></div>
             <div class="card-body text-center">
                 <a href="/profil">
-                    <img class="card-profile-img" :src="user.avatar">
+                    <img class="card-profile-img mb-3" :src="user.avatar">
                 </a>
-                <h6 class="card-title">
-                    <a class="text-inherit" href="/profil">{{ user.nickName }}</a>
-                </h6>
-                <p class="mb-4">{{ user.bio }}</p>
-                <ul class="card-menu">
+                <p class="mb-4" v-if="user.bio">{{ user.bio }}</p>
+                <ul class="card-menu mb-0">
                     <li class="card-menu-item">
                         Inlägg
                         <h6 class="my-0">{{ user.numberOfPosts }}</h6>
@@ -24,7 +21,10 @@
         </div>
         <div class="card mb-4 mb-4">
             <div class="card-body">
-                <h6 class="mb-3">Om</h6>
+                <h6 class="card-title">
+                    <a class="text-inherit" href="/profil">{{ user.nickName }}</a>
+                </h6>
+                <hr />
                 <ul class="list-unstyled list-spaced mb-0">
                     <li><span class="text-muted icon icon-thumbs-up mr-3"></span>{{ user.voteStatistics.upVotes }} givna upptummar</li>
                     <li><span class="text-muted icon icon-thumbs-down mr-3"></span>{{ user.voteStatistics.downVotes }} givna nertummar</li>
@@ -40,18 +40,6 @@
                 </ul>
             </div>
         </div>
-        <div class="card mb-4">
-            <div class="card-body">
-                <h6 class="mb-3">Länktipstoppen</h6>
-                <ul class="list-unstyled list-spaced mb-0" v-if="urlTips.length > 0">
-                    <li class="ellipsis" v-for="tip in urlTips" :key="tip.href + tip.clicks">
-                        <span class="text-muted icon" :class="icon(tip.href)"></span>
-                        <small class="ml-1"><span class="badge badge-secondary">{{ tip.clicks }}</span></small>
-                        <url :tip="tip" class="ml-1" />
-                    </li>
-                </ul>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -61,55 +49,25 @@ import { Component } from 'vue-property-decorator'
 import { Getter, Action, Mutation, State, namespace } from 'vuex-class'
 
 import { PageViewModel } from '../../../model/common'
-import { UrlTip } from '../../../model/forum'
 
 const ModuleGetter = namespace('forum', Getter)
 const ModuleAction = namespace('forum', Action)
 const ModuleMutation = namespace('forum', Mutation)
 
-import { GET_URLTIP_TOPLIST, GET_SHOW_LEFT_SIDEBAR, SET_SHOW_LEFT_SIDEBAR, FETCH_URLTIP_TOPLIST } from '../../../modules/forum/types'
+import { GET_SHOW_LEFT_SIDEBAR, SET_SHOW_LEFT_SIDEBAR } from '../../../modules/forum/types'
 
-import Url from '../../../components/url.vue'
-
-@Component({
-    components: {
-        Url
-    }
-})
+@Component({})
 export default class LeftSidebar extends Vue {
     @State(state => state.vm) vm: PageViewModel
-    @ModuleGetter(GET_URLTIP_TOPLIST) urlTips: UrlTip[]
     @ModuleGetter(GET_SHOW_LEFT_SIDEBAR) showLeftSideBar: boolean
-    @ModuleAction(FETCH_URLTIP_TOPLIST) loadUrlTipTopList: () => Promise<void>
     @ModuleMutation(SET_SHOW_LEFT_SIDEBAR) setShowLeftSideBar: (payload: { value: boolean }) => void
 
     mounted() {
         this.setShowLeftSideBar({ value: !this.vm.loggedInUser.filter.hideLeftMenu })
-        this.loadUrlTipTopList()
     }
 
     get user() {
         return this.vm.loggedInUser
-    }
-
-    icon(href: string): string {
-        if (href.includes('facebook'))
-            return 'icon-facebook'
-        if (href.includes('instagram'))
-            return 'icon-instagram'
-        if (href.includes('twitter'))
-            return 'icon-twitter'
-        if (href.includes('youtube'))
-            return 'icon-youtube'
-        if (href.includes('spotify'))
-            return 'icon-spotify'
-        if (href.includes('soundcloud'))
-            return 'icon-soundcloud'
-        if (href.includes('dif'))
-            return 'icon-heart'
-        if (href.includes('aik.se') || href.includes('aikfotboll.se'))
-            return 'icon-trash'
-        return 'icon-link'
     }
 }
 </script>
