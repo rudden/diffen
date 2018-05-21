fe835a6f69bba9c7b21641f309210d452a97a27f
Simon Rudén <ruden.simon@gmail.com>
2018-05-21 19:14:39 +0200

    Modify some migrations

--- Diffen/Migrations/20180520170306_NewDataOnGameAndPlayerEvent.cs
+++ Diffen/Migrations/20180520170306_NewDataOnGameAndPlayerEvent.cs
@@ -20,45 +20,9 @@ namespace Diffen.Migrations
                 table: "Games",
                 nullable: true);
 
-            migrationBuilder.CreateTable(
-                name: "GameToLineup",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    GameId = table.Column<int>(nullable: false),
-                    LineupId = table.Column<int>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_GameToLineup", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_GameToLineup_Games_GameId",
-                        column: x => x.GameId,
-                        principalTable: "Games",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_GameToLineup_Lineups_LineupId",
-                        column: x => x.LineupId,
-                        principalTable: "Lineups",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
             migrationBuilder.CreateIndex(
                 name: "IX_Games_LineupId",
                 table: "Games",
-                column: "LineupId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_GameToLineup_GameId",
-                table: "GameToLineup",
-                column: "GameId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_GameToLineup_LineupId",
-                table: "GameToLineup",
                 column: "LineupId");
 
             migrationBuilder.AddForeignKey(
@@ -75,9 +39,6 @@ namespace Diffen.Migrations
             migrationBuilder.DropForeignKey(
                 name: "FK_Games_Lineups_LineupId",
                 table: "Games");
-
-            migrationBuilder.DropTable(
-                name: "GameToLineup");
 
             migrationBuilder.DropIndex(
                 name: "IX_Games_LineupId",
