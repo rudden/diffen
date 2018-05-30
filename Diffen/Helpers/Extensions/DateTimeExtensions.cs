9fbf94b25abb5d69524837308268c29dba50abcb
Simon Rudén <ruden.simon@gmail.com>
2018-05-30 19:53:30 +0200

    Display date and time for posts when post is older than 1 minute

--- Diffen/Helpers/Extensions/DateTimeExtensions.cs
+++ Diffen/Helpers/Extensions/DateTimeExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Globalization;
 
 namespace Diffen.Helpers.Extensions
 {
@@ -7,28 +6,12 @@ namespace Diffen.Helpers.Extensions
 	{
 		public static string GetSinceStamp(this DateTime dt)
 		{
-			if (dt < DateTime.Now.AddDays(-1))
+			if (dt <= DateTime.Now.AddHours(-1))
 			{
-				if (Convert.ToInt32((DateTime.Now - dt).Days) > 7)
-				{
-					return dt.ToString("yyyy-MM-dd HH:mm");
-				}
-				return (DateTime.Now - dt).Days.ToString(CultureInfo.InvariantCulture) + " d sedan";
+				return dt.ToString("yyyy-MM-dd HH:mm");
 			}
-			if (dt > DateTime.Now.AddHours(-1))
-			{
-				var minutes = (DateTime.Now - dt).Minutes;
-				if (minutes <= 0)
-				{
-					return "alldeles nyss";
-				}
-				return (DateTime.Now - dt).Minutes.ToString(CultureInfo.InvariantCulture) + " m sedan";
-			}
-			if (dt > DateTime.Now.AddDays(-1))
-			{
-				return (DateTime.Now - dt).Hours.ToString(CultureInfo.InvariantCulture) + " t sedan";
-			}
-			return "";
+			var minutes = (DateTime.Now - dt).Minutes;
+			return minutes <= 0 ? "alldeles nyss" : dt.ToString("yyyy-MM-dd HH:mm");
 		}
 
 		public static string GetSinceStamp(this DateTime? dtn)
